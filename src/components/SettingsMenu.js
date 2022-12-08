444be5740b820576e891a8b5b306be1f30c8bbdb
Misty Becker <mistyb01@uw.edu>
2022-12-08 14:07:12 -0800

    created handleChange() for settings form

--- src/components/SettingsMenu.js
+++ src/components/SettingsMenu.js
@@ -1,9 +1,17 @@
 import React, { useState, useEffect } from 'react'
 
 function SettingsMenu() {
+    const [workInput, setWorkInput] = useState(25);
+    const [breakInput, setBreakInput] = useState(5);
 
     function handleSubmit(e) {
-       console.log(e); 
+       e.preventDefault();
+    }
+
+    function handleChange(e) {
+        let mins = parseInt(e.target.value);
+        e.target.id == 'work-time-input' ? setWorkInput(mins) 
+        : setBreakInput(mins);
     }
 
     return (
@@ -14,13 +22,13 @@ function SettingsMenu() {
                     <label htmlFor="work-time-input">
                         Work min
                     </label>
-                    <input id="work-time-input" type="number" />
+                    <input id="work-time-input" type="number" onChange={handleChange} />
                 </div>
                 <div className="form-field">
                     <label htmlFor="break-time-input">
                         Break min
                     </label>
-                    <input id="break-time-input" type="number" />
+                    <input id="break-time-input" type="number" onChange={handleChange} />
                 </div>
                 <button type="submit" className="form-button">save</button>
             </form>

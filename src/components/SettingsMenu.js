f6d35923462ca73fb0d036e9e781272a8fa09d0e
Misty Becker <mistyb01@uw.edu>
2022-12-08 13:35:03 -0800

    created static settings menu

--- src/components/SettingsMenu.js
+++ src/components/SettingsMenu.js
@@ -1,9 +1,29 @@
 import React, { useState, useEffect } from 'react'
 
 function SettingsMenu() {
+
+    function handleSubmit(e) {
+       console.log(e); 
+    }
+
     return (
-        <div className='settings'>
-        settings
+        <div className='settings flex-column-center'>
+            <h3>settings</h3>
+            <form onSubmit={handleSubmit} className='flex-column-center'>
+                <div className="form-field">
+                    <label htmlFor="work-time-input">
+                        Work min
+                    </label>
+                    <input id="work-time-input" type="number" />
+                </div>
+                <div className="form-field">
+                    <label htmlFor="break-time-input">
+                        Break min
+                    </label>
+                    <input id="break-time-input" type="number" />
+                </div>
+                <button type="submit" className="form-button">save</button>
+            </form>
         </div>
     );
 }
